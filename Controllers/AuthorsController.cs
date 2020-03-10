4fcb953fbed35ddf3b26457bfced995d06dea7f9
wturon <59670834+wturon@users.noreply.github.com>
2020-03-10 12:54:29 -0400

    author-controller-updates (#2)

--- Controllers/AuthorsController.cs
+++ Controllers/AuthorsController.cs
@@ -1,8 +1,73 @@
+using Fisher.Bookstore.Models;
+using Fisher.Bookstore.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Fisher.Bookstore.Controllers
 {
-
-    public class AuthorsController
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
     {
+        private IAuthorsRepository authorsRepository;
+
+        public AuthorsController(IAuthorsRepository repository)
+        {
+            authorsRepository = repository;
+        } 
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            return Ok(authorsRepository.GetAuthors());
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult Get(int authorId)
+        {
+            if(!authorsRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            return Ok(authorsRepository.GetAuthor(authorId));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Author author)
+        {
+            var authorId = authorsRepository.AddAuthor(author);
+            return Created($"https://localhost:5001/api/authors/{authorId}", author);
+        }
+
+        [HttpPut("{authorId}")]
+        public IActionResult Put(int authorId,[FromBody]Author author)
+        {
+            if(author.Id != authorId)
+            {
+                return BadRequest(); 
+            }
+           
+            if(!authorsRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            authorsRepository.UpdateAuthor(author);
+            return Ok(author);
+        }
+
+        [HttpDelete("{authorId}")]
+        public IActionResult Delete(int authorId)
+        {
+            if(!authorsRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            authorsRepository.DeleteAuthor(authorId);
+            return Ok();
+        }
+
 
     }
 }
